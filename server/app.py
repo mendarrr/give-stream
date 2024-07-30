2779ba357040c9d1a099bffd921687fbd7bae354
mendarrr <sarahabby541@gmail.com>
2024-07-30 20:59:50 +0300

    Updating code

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from datetime import datetime
@@ -14,9 +14,12 @@ migrate = Migrate(app, db)
 db.init_app(app)
 api = Api(app)
 
-@app.route('/')
-def home():
-    return "Welcome to the Charity Donation Platform"
+class Index(Resource):
+    def get(self):
+        response_dict = {"index": "Welcome to the GiveStream API"}
+        return make_response(response_dict, 200)
+
+api.add_resource(Index, '/')
 
 class DonationResource(Resource):
     # Method used to get all donation records from the databasedef get(self, donor_id=None, charity_id=None)
