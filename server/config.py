966d9ca64194ebc6d71e5ebed7bbdb22f3e001a8
mendarrr <sarahabby541@gmail.com>
2024-08-02 12:31:18 +0300

    Adding a new migration with the new tables added and seeded data

--- server/config.py
+++ server/config.py
@@ -11,7 +11,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///give_stream.db'
 app.secret_key = b"\xa4\x82\x9fs\xf2\x81\xa4'&\xfd\xf1\x07\xe2\x1b>\xc7"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
--- server/migrations/versions/f95603890f6c_initial_migration.py
+++ server/migrations/versions/f95603890f6c_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration
 
-Revision ID: b0fa9f75a3a4
+Revision ID: f95603890f6c
 Revises: 
-Create Date: 2024-07-30 01:28:01.884704
+Create Date: 2024-08-02 12:27:42.248432
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'b0fa9f75a3a4'
+revision = 'f95603890f6c'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -22,6 +22,7 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=64), nullable=False),
     sa.Column('email', sa.String(length=120), nullable=False),
+    sa.Column('_password_hash', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
@@ -31,6 +32,7 @@ def upgrade():
     sa.Column('username', sa.String(length=64), nullable=False),
     sa.Column('email', sa.String(length=120), nullable=False),
     sa.Column('name', sa.String(length=128), nullable=False),
+    sa.Column('_password_hash', sa.String(), nullable=True),
     sa.Column('description', sa.Text(), nullable=True),
     sa.Column('needed_donation', sa.Float(), nullable=True),
     sa.PrimaryKeyConstraint('id'),
@@ -38,20 +40,18 @@ def upgrade():
     sa.UniqueConstraint('name'),
     sa.UniqueConstraint('username')
     )
-    op.create_table('donors',
+    op.create_table('payment_methods',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('username', sa.String(length=64), nullable=False),
-    sa.Column('email', sa.String(length=120), nullable=False),
-    sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('email'),
-    sa.UniqueConstraint('username')
+    sa.Column('name', sa.String(length=50), nullable=False),
+    sa.Column('description', sa.String(length=255), nullable=True),
+    sa.PrimaryKeyConstraint('id')
     )
     op.create_table('beneficiaries',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('charity_id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=128), nullable=False),
     sa.Column('description', sa.Text(), nullable=True),
-    sa.ForeignKeyConstraint(['charity_id'], ['charities.id'], ),
+    sa.ForeignKeyConstraint(['charity_id'], ['charities.id'], name=op.f('fk_beneficiaries_charity_id_charities')),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('charity_applications',
@@ -63,31 +63,29 @@ def upgrade():
     sa.Column('submission_date', sa.DateTime(), nullable=True),
     sa.Column('reviewed_by', sa.Integer(), nullable=True),
     sa.Column('review_date', sa.DateTime(), nullable=True),
-    sa.ForeignKeyConstraint(['reviewed_by'], ['admins.id'], ),
+    sa.ForeignKeyConstraint(['reviewed_by'], ['admins.id'], name=op.f('fk_charity_applications_reviewed_by_admins')),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email')
     )
-    op.create_table('donations',
+    op.create_table('donors',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('donor_id', sa.Integer(), nullable=False),
-    sa.Column('charity_id', sa.Integer(), nullable=False),
-    sa.Column('amount', sa.Float(), nullable=False),
-    sa.Column('date', sa.DateTime(), nullable=True),
+    sa.Column('username', sa.String(length=64), nullable=False),
+    sa.Column('email', sa.String(length=120), nullable=False),
+    sa.Column('_password_hash', sa.String(), nullable=True),
     sa.Column('is_anonymous', sa.Boolean(), nullable=True),
-    sa.Column('is_recurring', sa.Boolean(), nullable=True),
-    sa.Column('recurring_frequency', sa.String(length=20), nullable=True),
-    sa.Column('next_donation_date', sa.DateTime(), nullable=True),
-    sa.ForeignKeyConstraint(['charity_id'], ['charities.id'], ),
-    sa.ForeignKeyConstraint(['donor_id'], ['donors.id'], ),
-    sa.PrimaryKeyConstraint('id')
+    sa.Column('payment_method_id', sa.Integer(), nullable=True),
+    sa.ForeignKeyConstraint(['payment_method_id'], ['payment_methods.id'], name=op.f('fk_donors_payment_method_id_payment_methods')),
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('email'),
+    sa.UniqueConstraint('username')
     )
-    op.create_table('inventory',
+    op.create_table('inventories',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('charity_id', sa.Integer(), nullable=False),
     sa.Column('item_name', sa.String(length=128), nullable=False),
     sa.Column('quantity', sa.Integer(), nullable=False),
-    sa.Column('date_updated', sa.DateTime(), nullable=True),
-    sa.ForeignKeyConstraint(['charity_id'], ['charities.id'], ),
+    sa.Column('last_updated', sa.DateTime(), nullable=True),
+    sa.ForeignKeyConstraint(['charity_id'], ['charities.id'], name=op.f('fk_inventories_charity_id_charities')),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('stories',
@@ -96,7 +94,23 @@ def upgrade():
     sa.Column('title', sa.String(length=128), nullable=False),
     sa.Column('content', sa.Text(), nullable=False),
     sa.Column('date_posted', sa.DateTime(), nullable=True),
-    sa.ForeignKeyConstraint(['charity_id'], ['charities.id'], ),
+    sa.ForeignKeyConstraint(['charity_id'], ['charities.id'], name=op.f('fk_stories_charity_id_charities')),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('donations',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('donor_id', sa.Integer(), nullable=False),
+    sa.Column('charity_id', sa.Integer(), nullable=False),
+    sa.Column('payment_method_id', sa.Integer(), nullable=False),
+    sa.Column('amount', sa.Float(), nullable=False),
+    sa.Column('date', sa.DateTime(), nullable=True),
+    sa.Column('is_anonymous', sa.Boolean(), nullable=True),
+    sa.Column('is_recurring', sa.Boolean(), nullable=True),
+    sa.Column('recurring_frequency', sa.String(length=20), nullable=True),
+    sa.Column('next_donation_date', sa.DateTime(), nullable=True),
+    sa.ForeignKeyConstraint(['charity_id'], ['charities.id'], name=op.f('fk_donations_charity_id_charities')),
+    sa.ForeignKeyConstraint(['donor_id'], ['donors.id'], name=op.f('fk_donations_donor_id_donors')),
+    sa.ForeignKeyConstraint(['payment_method_id'], ['payment_methods.id'], name=op.f('fk_donations_payment_method_id_payment_methods')),
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
@@ -104,12 +118,13 @@ def upgrade():
 
 def downgrade():
     # ### commands auto generated by Alembic - please adjust! ###
-    op.drop_table('stories')
-    op.drop_table('inventory')
     op.drop_table('donations')
+    op.drop_table('stories')
+    op.drop_table('inventories')
+    op.drop_table('donors')
     op.drop_table('charity_applications')
     op.drop_table('beneficiaries')
-    op.drop_table('donors')
+    op.drop_table('payment_methods')
     op.drop_table('charities')
     op.drop_table('admins')
     # ### end Alembic commands ###
