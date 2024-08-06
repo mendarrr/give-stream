bf1cd4a0746b029052b88b201c1bb87314433cee
mendarrr <sarahabby541@gmail.com>
2024-08-06 12:21:14 +0300

    Updating the Charity Card Layout

--- client/src/components/CharityCard.js
+++ client/src/components/CharityCard.js
@@ -3,17 +3,20 @@ import './CharityCard.css';
 
 const CharityCard = ({ charity }) => {
     return (
-        <div className="charity-card">
-            <h2>{charity.name}</h2>
-            <p>Amount Raised: ${charity.total_raised}</p>
-            <p>Number of Donations: {charity.donation_count}</p>
-            <div className="progress-bar">
-                <div 
-                    className="progress" 
-                    style={{width: `${charity.percentage_raised}%`}}
-                ></div>
+        <div>
+            <div className='charity-profile'></div>
+            <div className="charity-card">
+                <h2>{charity.name}</h2>
+                <p>Amount Raised: ${charity.total_raised}</p>
+                <p>Number of Donations: {charity.donation_count}</p>
+                <div className="progress-bar">
+                    <div 
+                        className="progress" 
+                        style={{width: `${charity.percentage_raised}%`}}
+                    ></div>
+                </div>
+                <p>Goal: ${charity.total_raised} / ${charity.needed_donation}</p>
             </div>
-            <p>Goal: ${charity.total_raised} / ${charity.needed_donation}</p>
         </div>
     );
 };
