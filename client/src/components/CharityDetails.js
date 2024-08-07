a89a875198713c3096f632a5db23a6dbbb110223
Kamau-sam <skamau.6411@gmail.com>
2024-08-07 11:10:22 +0300

    added API calls for success stories and donations, and modified server-side Python code

--- client/src/components/CharityDetails.js
+++ client/src/components/CharityDetails.js
@@ -4,23 +4,52 @@ import "./CharityDetails.css";
 
 function CharityDetails() {
   const [charity, setCharity] = useState(null);
+  const [successStories, setSuccessStories] = useState([]);
+  const [donations, setDonations] = useState([]);
+  const [comments, setComments] = useState([
+    {
+      name: "Jane Doe",
+      amount: 5000,
+      time: "2 hours ago",
+      comment: "This is a great cause!",
+    },
+    {
+      name: "John Smith",
+      amount: 2000,
+      time: "1 day ago",
+      comment: "Happy to contribute!",
+    },
+    {
+      name: "Emily Johnson",
+      amount: 750,
+      time: "3 hours ago",
+      comment: "Glad to support this charity. Keep up the great work!",
+    },
+    {
+      name: "Lucas Wilson",
+      amount: 800,
+      time: "2 days ago",
+      comment: "Every little bit helps. Keep making a difference!",
+    },
+    {
+      name: "Olivia Martinez",
+      amount: 1200,
+      time: "3 days ago",
+      comment: "Inspired by your mission. Happy to contribute!",
+    },
+  ]);
   const [error, setError] = useState(null);
   const { id } = useParams();
-  const [currentStoryIndex, setCurrentStoryIndex] = useState(0);
   const carouselRef = useRef(null);
 
   useEffect(() => {
     const fetchCharityDetails = async () => {
       try {
-        console.log(`Fetching charity with ID: ${id}`);
         const response = await fetch(`http://127.0.0.1:5000/charities/${id}`);
-
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
         const data = await response.json();
-        console.log("Fetched data:", data);
         setCharity(data);
       } catch (error) {
         console.error("Error fetching charity details:", error);
@@ -28,99 +57,54 @@ function CharityDetails() {
       }
     };
 
+    const fetchSuccessStories = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/stories");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setSuccessStories(data);
+      } catch (error) {
+        console.error("Error fetching success stories:", error);
+        setError("Failed to fetch success stories. Please try again later.");
+      }
+    };
+
+    const fetchDonations = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/donations");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setDonations(data);
+      } catch (error) {
+        console.error("Error fetching donations:", error);
+        setError("Failed to fetch donations. Please try again later.");
+      }
+    };
+
     fetchCharityDetails();
+    fetchSuccessStories();
+    fetchDonations();
   }, [id]);
-
-  const successStories = [
-    {
-      title: "SDA Mbotela Camp Meeting Expenses",
-      raised: 331200,
-      donations: 591,
-      goal: 600000,
-      image: "path_to_image1.jpg",
-    },
-    {
-      title: "Qiya's Journey of Hope",
-      raised: 255300,
-      donations: 132,
-      goal: 1000000,
-      image: "path_to_image2.jpg",
-    },
-    {
-      title: "Makongeni Camp 2024",
-      raised: 611945,
-      donations: 1203,
-      goal: 700000,
-      image: "path_to_image3.jpg",
-    },
-    {
-      title: "Feeding Program 2024",
-      raised: 450000,
-      donations: 789,
-      goal: 800000,
-      image: "path_to_image4.jpg",
-    },
-  ];
-
-  const comments = [
-    {
-      name: "Umbra Foundation",
-      amount: 15000,
-      comment: "Hope the kids get their food",
-      time: "1 hr",
-    },
-    {
-      name: "Amy Bhoke",
-      amount: 5000,
-      comment: "When I saw this I had to make a contribution",
-      time: "1 d",
-    },
-    {
-      name: "Mary Omondi",
-      amount: 2000,
-      comment: "Lovely",
-      time: "2 d",
-    },
-    {
-      name: "Naman Parkesh",
-      amount: 3000,
-      comment: "Let's support our children",
-      time: "2 d",
-    },
-    {
-      name: "Hadid Faraka",
-      amount: 500,
-      comment: "Hope this helps, keep sending updates our way",
-      time: "2 d",
-    },
-    {
-      name: "Sylvia Nefartiti",
-      amount: 100,
-      comment:
-        "It's not much but I hope my contribution makes a difference no matter how small it may be.",
-      time: "2 d",
-    },
-  ];
 
   const scrollCarousel = (direction) => {
     if (carouselRef.current) {
       const scrollAmount = carouselRef.current.offsetWidth;
-      if (direction === "left") {
-        carouselRef.current.scrollBy({
-          left: -scrollAmount,
-          behavior: "smooth",
-        });
-      } else {
-        carouselRef.current.scrollBy({
-          left: scrollAmount,
-          behavior: "smooth",
-        });
-      }
+      carouselRef.current.scrollBy({
+        left: direction === "left" ? -scrollAmount : scrollAmount,
+        behavior: "smooth",
+      });
     }
   };
 
   if (error) return <div className="error-message">{error}</div>;
-  if (!charity) return <div>Loading...</div>;
+  if (!charity || donations.length === 0 || successStories.length === 0)
+    return <div>Loading...</div>;
+
+  const formatNumber = (number) => (number ? number.toLocaleString() : "0");
 
   return (
     <div className="charity-details">
@@ -128,8 +112,8 @@ function CharityDetails() {
       <div className="charity-content">
         <div className="charity-info">
           <div className="progress-card">
-            <h3>KES {charity.raisedAmount.toLocaleString()}</h3>
-            <p>raised of KES {charity.goalAmount.toLocaleString()} goal</p>
+            <h3>KES {formatNumber(charity.raisedAmount)}</h3>
+            <p>raised of KES {formatNumber(charity.goalAmount)} goal</p>
             <div className="progress-bar">
               <div
                 className="progress"
@@ -140,23 +124,24 @@ function CharityDetails() {
                 }}
               ></div>
             </div>
-            <p>{charity.donationCount.toLocaleString()} donations</p>
+            <p>{formatNumber(charity.donationCount)} donations</p>
             <button className="donate-button">Donate Now</button>
             <div className="recent-activity">
               <p>{charity.recentDonationCount} people just donated</p>
             </div>
             <div className="recent-donors">
-              {charity.recentDonors &&
-                charity.recentDonors.map((donor, index) => (
-                  <div key={index} className="donor">
-                    <span className="donor-initial">{donor.name[0]}</span>
-                    <span className="donor-name">{donor.name}</span>
-                    <span className="donor-amount">
-                      KES {donor.amount.toLocaleString()}
-                    </span>
-                    <span className="donation-type">{donor.type}</span>
-                  </div>
-                ))}
+              {donations.map((donation, index) => (
+                <div key={index} className="donor">
+                  <span className="donor-initial">
+                    {donation.name && donation.name[0]}
+                  </span>
+                  <span className="donor-name">{donation.name}</span>
+                  <span className="donor-amount">
+                    KES {formatNumber(donation.amount)}
+                  </span>
+                  <span className="donation-type">{donation.type}</span>
+                </div>
+              ))}
             </div>
             <div className="share-fundraiser">
               <h4>Share this fundraiser</h4>
@@ -196,7 +181,6 @@ function CharityDetails() {
             src={charity.imageUrl}
             alt={charity.name}
             className="charity-image"
-            style={{ width: "25%" }}
           />
           <p>{charity.organizer} is organizing this fundraiser.</p>
           <div className="donation-protected">
@@ -218,8 +202,8 @@ function CharityDetails() {
                   <div key={index} className="story-card">
                     <img src={story.image} alt={story.title} />
                     <h4>{story.title}</h4>
-                    <p>RAISED: KES {story.raised.toLocaleString()}</p>
-                    <p>Donations: {story.donations}</p>
+                    <p>RAISED: KES {formatNumber(story.raised)}</p>
+                    <p>Donations: {formatNumber(story.donations)}</p>
                     <div className="progress-bar">
                       <div
                         className="progress"
@@ -229,8 +213,8 @@ function CharityDetails() {
                       ></div>
                     </div>
                     <p>
-                      KES {story.raised.toLocaleString()} funds raised of KES{" "}
-                      {story.goal.toLocaleString()} goal
+                      KES {formatNumber(story.raised)} funds raised of KES{" "}
+                      {formatNumber(story.goal)} goal
                     </p>
                     <button className="donate-button">DONATE</button>
                   </div>
@@ -252,7 +236,7 @@ function CharityDetails() {
                   <span className="commenter-initial">{comment.name[0]}</span>
                   <span className="commenter-name">{comment.name}</span>
                   <span className="comment-amount">
-                    KES {comment.amount.toLocaleString()}
+                    KES {formatNumber(comment.amount)}
                   </span>
                   <span className="comment-time">{comment.time}</span>
                 </div>
