437ca815f5856742257df42ff1c436eaee905b9c
mendarrr <sarahabby541@gmail.com>
2024-08-07 09:27:09 +0300

    Styling the footer and adding a footer section in the home page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,67 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Footer.css'
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer className="footer">
+      <div className="footer-middle">
+        <div className="column">
+        <img src={`${process.env.PUBLIC_URL}/GiveStreamLogo.png`} alt="logo" />
+          <p>Give Stream is an Online Fundraising platform that connects Donors deserving charities
+            and enables easy and secure charitable donations. We provide the most Secure, Transparent 
+            and convenient way for different charities to find donors for Medical Bills, Construction, 
+            Food relief, Emergencies, Funerals, School Fees and many more
+          </p>
+          <div className="social-links">
+            <a href="#"><i className="fab fa-facebook-f"></i></a>
+            <a href="#"><i className="fab fa-twitter"></i></a>
+            <a href="#"><i className="fab fa-instagram"></i></a>
+            <a href="#"><i className="fab fa-youtube"></i></a>
+            <a href="#"><i className="fab fa-linkedin-in"></i></a>
+          </div>
+          {/* <p>Get the app:</p>
+          <div className="app-links">
+            <a href="#">App Store</a>
+            <a href="#">Google Play</a>
+          </div> */}
+        </div>
+        <div className="column menu">
+          <h3>MENU</h3>
+          <nav>
+            <ul>
+              <li><Link to="/questions">Questions</Link></li>
+              <li><Link to="/setup-campaign">Set up Donation Campaign</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/faq">FAQ</Link></li>
+              <li><Link to="/communities">Communities</Link></li>
+              <li><Link to="/givestream-communities">Give Stream communities</Link></li>
+            </ul>
+          </nav>
+        </div>
+        <div className="column contact">
+          <h3>CONTACT</h3>
+          <p>123 Charity Lane, Nairobi, Kenya</p>
+          <p>Phone: +254 123 456 789</p>
+          <p>Email: info@givestream.com</p>
+          <div className="country-badge">
+            <span>🇰🇪</span><p>Kenya, English</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='border-top'></div>
+
+      <div className="footer-bottom">
+        <div className="copyright">
+          &copy; 2024 Give Stream. All Rights Reserved.
+        </div>
+        <div className="legal-links">
+          <Link to="/privacy-policy">Privacy Policy</Link>
+          <Link to="/terms-of-use" className='termss'>Terms of Use</Link>
+        </div>
+      </div>
+    </footer>
   )
 }
 
