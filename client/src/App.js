c1a1a53c5d73fd3b4c4ab6ef14a488d589eeec65
Kamau-sam <skamau.6411@gmail.com>
2024-08-08 11:32:02 +0300

    deleted HomePage component and css

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-// Import components
-import HomePage from "./components/Homepage";
 import CharityList from "./components/CharityList";
 import CharityDetails from "./components/CharityDetails";
 import CharityProfile from "./components/CharityProfile";
@@ -14,7 +12,6 @@ function App() {
     <Router>
       <div className="app-container">
         <Routes>
-          <Route path="/" element={<HomePage />} />
           <Route path="/charities" element={<CharityList />} />
           <Route path="/charity/:id" element={<CharityDetails />} />
           <Route path="/charity-profile/:id" element={<CharityProfile />} />
