55e1d5758b1ec733913ce4e46f2cc59dd578f091
mendarrr <sarahabby541@gmail.com>
2024-08-14 08:13:16 +0300

    "Deleted migration files and their corresponding pycache files."

--- server/migrations/versions/7932004248c5_reinitialising_the_database.py
+++ server/migrations/versions/7932004248c5_reinitialising_the_database.py
@@ -1,8 +1,8 @@
-"""reinitializing database
+"""Reinitialising the database
 
-Revision ID: 63b180a45412
+Revision ID: 7932004248c5
 Revises: 
-Create Date: 2024-08-13 14:50:38.571461
+Create Date: 2024-08-14 08:09:51.167119
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '63b180a45412'
+revision = '7932004248c5'
 down_revision = None
 branch_labels = None
 depends_on = None
