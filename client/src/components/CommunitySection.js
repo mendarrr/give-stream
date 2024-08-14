dde9fcf6d2fdc7dab4202aa63cc7f62dc7350131
mendarrr <sarahabby541@gmail.com>
2024-08-14 12:08:18 +0300

    "Updated CSS selectors in CharitiesPage.css and added import statement in CommunitySection.js"

--- client/src/components/CommunitySection.js
+++ client/src/components/CommunitySection.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
+import "./CharityCard.css"
 
 const CommunitiesSection = () => {
   const [searchTerm, setSearchTerm] = useState("");
